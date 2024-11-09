b6c79b70c416ca82aabf8709f972c00d156b6fd4
Athul-Ramesan <135419747+Athul-Ramesan@users.noreply.github.com>
2024-11-09 15:10:54 +0530

    Displayed invitation to users (#6)
    * feat: added display invitation elements as hidden
    
    * feat: added display invitation functionality with formated date
    
    * feat: added functionality to format time to AM or PM

---
+++ src/utils/displayInvitaion.js
@@ -0,0 +1,45 @@
+
+function displayInvitaion(formData) {
+    document.getElementById('form-container').style.display = "none"
+
+    const eventName = formData.get('eventName')
+    const eventDate = formData.get('eventDate')
+    const startTime = formData.get('startTime')
+    const endTime = formData.get('endTime')
+    console.log("🚀 ~ displayInvitaion ~ endTime:", endTime)
+    const eventDescription = formData.get('eventDescription')
+    const eventLocation = formData.get('eventLocation')
+
+    const formattedDate = formatDateWithYearMonthDay(eventDate)
+    const formattedStartTime = formatTimeToAMorPM(startTime)
+    const formattedEndTime = formatTimeToAMorPM(endTime)
+
+    document.querySelector('.event-name').textContent = eventName;
+    document.querySelector('.event-date').textContent = formattedDate;
+    document.querySelector('.event-time').textContent = `${formattedStartTime} - ${formattedEndTime}`;
+    document.querySelector('.event-description').textContent = eventDescription;
+    document.querySelector('.event-location').textContent = eventLocation;
+
+
+    document.getElementById('invitation-display').classList.remove('hidden-modal')
+}
+function formatDateWithYearMonthDay(date) {
+    const newDate = new Date(date);
+
+    const formattedDate = new Intl.DateTimeFormat('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }).format(newDate);
+
+    return formattedDate
+}
+function formatTimeToAMorPM(time){
+    let [hours,minutes] = time.split(":")
+    const timePeriod = Number(hours) >= 12 ? "PM" : "AM"
+    hours = hours % 12 || 12; 
+    const formattedTime = `${hours}:${minutes.toString().padStart(2, '0')} ${timePeriod}`;
+
+    return formattedTime
+}
