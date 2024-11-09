a036cffc48554bac74eec1e74ea511881fe470f9
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2024-11-09 10:42:33 +0530

    refactor: updated head content

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 
 function app(){
-    const headerText = document.getElementById("header-text")
-    headerText.innerText = "Event Invitation Creator"
+    
 }
 document.addEventListener("DOMContentLoaded", app)
