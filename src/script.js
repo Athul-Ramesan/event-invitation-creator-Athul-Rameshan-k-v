a75375ec7cf67180a13ef083a30f2207e0a1a2cd
Athul-Ramesan <135419747+Athul-Ramesan@users.noreply.github.com>
2024-11-09 13:49:37 +0530

    Added input and validation (#4)
    * feat: added form input for event creation
    
    * feat: added validation function for empty field
    
    * feat: added message showing modal for validation

--- src/script.js
+++ src/script.js
@@ -1,5 +1,11 @@
 
-function app(){
-    
+function app() {
+    const eventForm = document.getElementById('event-form')
+    const today = new Date().toISOString().split('T')[0]
+    const eventDateInput = document.getElementById('event-date');
+    eventDateInput.setAttribute('min', today);
+    ("event-form")
+    console.log("🚀 ~ app ~ eventForm:", eventForm)
+    eventForm.addEventListener("submit", handleSubmit)
 }
 document.addEventListener("DOMContentLoaded", app)
